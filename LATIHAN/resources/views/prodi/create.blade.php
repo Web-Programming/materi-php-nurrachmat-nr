80292aacdb74d208b0ad5bdc24e96e634ba7c786
Nur Rachmat <rachmat.nur91@gmail.com>
2025-05-17 18:35:02 +0700

    CRUD : Insert data

--- LATIHAN/resources/views/prodi/create.blade.php
+++ LATIHAN/resources/views/prodi/create.blade.php
@@ -55,30 +55,29 @@
                     </div>
                   </div>
                   <div class="card-body">
-
-                    <form method="post" action="{{ url("post") }}">
+                    @if (session('status'))
+                      <div class="alert alert-success">
+                          {{ session('status') }}
+                      </div>
+                    @endif
+                    <form method="post" action="{{ url("prodi") }}">
                       @csrf
                       <div class="mb-3">
                         <label >Nama Prodi</label>
-                        <input type="text" name="nama" class="form-control">
+                        <input type="text" name="nama" class="form-control" value="{{ old('nama') }}">
+                        @error('nama')
+                          <div class="text-danger">{{ $message }}</div>
+                        @enderror
                       </div>
                       <div class="mb-3">
                         <label>Kode Prodi</label>
-                        <input type="text" name="kode_prodi" class="form-control">
+                        <input type="text" name="kode_prodi" class="form-control" value="{{ old('kode_prodi') }}">
+                        @error('kode_prodi')
+                          <div class="text-danger">{{ $message }}</div>
+                        @enderror
                       </div>
                       <button type="submit" class="btn btn-primary">Simpan</button>
                     </form>
-
-
-
-
-
-
-
-
-
-
-                    
                   </div>
                   <!-- /.card-body -->
                   <div class="card-footer">Footer</div>
